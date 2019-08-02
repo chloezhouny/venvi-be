fe7ba49b28e95aa1cf06bd8b77b2b4742d861956
Esar Behlum <esarnb@gmail.com>
2019-08-02 12:02:35 -0700

    Adjust root route

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,20 @@
-  const path = require("path");
-  const router = require("express").Router();
+const path = require("path");
+const router = require("express").Router();
 
-  const apiRoutes = require("./api");
-  const userRoutes = require("./auth")
+const apiRoutes = require("./api");
+const userRoutes = require("./auth")
 
-  router.use("/api", apiRoutes);
-  router.use("/auth", userRoutes);
+router.use("/api", apiRoutes);
+router.use("/auth", userRoutes);
 
-  router.use("/")
-    .get(function(req, res) {
-      res.sendFile(path.join(__dirname, "../client/build/index.html"));
-    });
+router.route("/")
+  .get(function (req, res) {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
 
-  module.exports = router;
+router.route("*")
+  .get(function (req, res) {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
+
+module.exports = router;
