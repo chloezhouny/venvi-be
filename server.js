adfda62da067cce519846e60bd462ad1b2f5b9cd
Esar Behlum <esarnb@gmail.com>
2019-08-03 12:26:11 -0700

    added button to html, put passportjs to proper config folder

--- server.js
+++ server.js
@@ -1,42 +1,18 @@
-
-const express = require("express");
-
-const routes = require("./routes");
-
 //Initialize Express
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-var db = require("./models");
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Add routes, both API and view
+// Add all routes to be used
+const routes = require("./routes");
 app.use(routes);
 
-// app.get("/", function(req, res) {
-//   console.log("SERVER GET ROUTE HIT");
-//   res.json({status: true, message: "CLIENT GET RECEIVED"})
-// })
-
-// app.post("/", function(req, res) {
-//   console.log("RECEIVED CLIENT REQ.BODY: ", req.body);
-//   res.json({status: "ok", msg: "Got your post request"})
-// })
-
-// app.put("/:id", function(req, res) {
-//   console.log(req.params.id);
-//   res.json({status: "ok", msg: `Got your put request with id ${req.params.id}`})
-// })
-
-// app.delete("/:id", function(req, res) {
-//   console.log(req.params.id);
-//   res.json({status: "ok", msg: `Got your delete request with id ${req.params.id}`})
-// })
-
 // Start the API server
+var db = require("./models");
 db.sequelize.sync().then(function() {
 	app.listen(PORT, function() {
 	  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
