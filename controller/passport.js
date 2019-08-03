f8a506276f020675eafb2b1a07bfed77a8acf95f
Esar Behlum <esarnb@gmail.com>
2019-08-03 12:17:26 -0700

    Using google strategy sign in, creating user for sign up

--- controller/passport.js
+++ controller/passport.js
@@ -20,4 +20,48 @@ module.exports = function(passport) {
     })
   })
 
+  //Google Strategy
+  passport.use(new GoogleStrategy({
+    clientID: configAuth.googleAuth.clientID,
+    clientSecret: configAuth.googleAuth.clientSecret,
+    callbackURL: configAuth.googleAuth.callbackURL
+  }, function(token, refreshToken, profile, done) {
+    process.nextTick(function() {
+      console.log("USER's PROFILE ID", profile.id);
+      
+      db.User.findOne({ where: {profileID: profile.id}}).then((user, err) => {
+        if (err) return done(err);
+        
+        if (user) return done(null, user)
+        else {
+          console.log("CREATING NEW USER: ", user);
+          db.User.create({
+            name: "NewName",
+            username: "NewUsername",
+            password: "NoPassword",
+            email: "sr@gmail.com",
+            profileID: profile.id
+          }).then((newUser) => {
+            if (!newUser) {
+              console.log("RETURNING NULL USER");
+              return done(null, false);
+            }
+            console.log("RETURNING NEW USER");
+            return done(null, newUser)
+          })
+          // var newUser = new db.User();
+
+          // newUser.google.id    = profile.id;
+          // newUser.google.token = token;
+          // newUser.google.name  = profile.displayName;
+          // newUser.google.email = profile.emails[0].value; // pull the first email
+
+          // newUser.save(function(err) {
+          //   if (err) throw err;
+          //   return done(null, newUser);
+          // });
+        }
+      });
+    });
+  }));
 }
