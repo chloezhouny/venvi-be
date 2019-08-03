65d317ed88dbda44e704e573c94d980dc75afc3d
Esar Behlum <esarnb@gmail.com>
2019-08-03 12:33:06 -0700

    express middleware and passport initialization with cookies and sessions

--- server.js
+++ server.js
@@ -3,13 +3,48 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Define middleware here
-app.use(express.urlencoded({ extended: true }));
+// Defining middleware 
+
+var morgan = require('morgan');
+var passport = require('passport');
+var flash = require('connect-flash');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+
+//Initialize passport.js from config
+require("./config/passport")(passport); // pass passport for configuration
+
+app.use(flash()); 
+app.use(morgan('dev')); 
+app.use(cookieParser()); 
 app.use(express.json());
+app.use(passport.session()); 
+app.use(passport.initialize());
+app.use(session({ secret: 'venividivenvi' }));
+app.use(express.urlencoded({ extended: true }));
+
+//Using CORS for heroku
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://venvi-be.herokuapp.com/');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
+//Send passport through express
+app.set('passport', passport);
 
 // Add all routes to be used
 const routes = require("./routes");
 app.use(routes);
+
+// Serve up static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+} else {
+  app.use(express.static("client/public"));
+}
 
 // Start the API server
 var db = require("./models");
