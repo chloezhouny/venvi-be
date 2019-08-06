61c705c6e1543ead54086af57988271aa13f1a69
Chloe Zhou <chloezhouny@gmail.com>
2019-08-05 18:34:25 -0700

    Test heroku

--- server.js
+++ server.js
@@ -43,6 +43,8 @@ app.use(express.urlencoded({ extended: true }));
 //Using CORS for heroku
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://venvi-be.herokuapp.com/');
+  // res.header('Access-Control-Allow-Origin', 'https://venvi-passport2.herokuapp.com/');
+  // res.header('Access-Control-Allow-Origin', 'http://localhost:3001');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
