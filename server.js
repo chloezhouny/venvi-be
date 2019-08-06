784c2f7e17f39e4ec5a8b94e1daf84591fb57564
Esar Behlum <esarnb@gmail.com>
2019-08-05 22:15:17 -0700

    removing multiple cors

--- server.js
+++ server.js
@@ -2,18 +2,8 @@
 const express = require("express");
 const app = express();
 
-var host = process.env.HOST || '0.0.0.0';
-
 const PORT = process.env.PORT || 3001;
 
-var cors_proxy = require('cors-anywhere');
-cors_proxy.createServer({
-    originWhitelist: [], // Allow all origins
-    requireHeader: ['origin', 'x-requested-with'],
-    removeHeaders: ['cookie', 'cookie2']
-}).listen(PORT, host, function() {
-    console.log('Running CORS Anywhere on ' + host + ':' + PORT);
-});
 // Defining middleware 
 
 var morgan = require('morgan');
