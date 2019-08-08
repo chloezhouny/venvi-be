d38c2cc2b8c497de49886d0dd186985f649f80af
Esar Behlum <esarnb@gmail.com>
2019-08-08 09:33:59 -0700

    updating routes to passportjs cookies

--- routes/auth/authRoutes.js
+++ routes/auth/authRoutes.js
@@ -2,32 +2,67 @@ var passport = require("passport");
 const router = require("express").Router();
 const authController = require("../../controller/user-controller");
 
-//First the button is hit to update passport
-router.route("/login").get( (req, res) => {
-  passport = req.app.get('passport')
-  res.redirect("/auth/google")
-})
+serialize = function(obj) {
+  var str = [];
+  for (var p in obj)
+    if (obj.hasOwnProperty(p)) {
+      str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+    }
+  return str.join("&");
+}
+
 
 //Then the user redirects to youtube
-router.route("/").get(
-  passport.authenticate('google', { scope: ["profile", "email"] })
-)
+router.get("/", (req, res) => {
+  passport.authenticate("google", { scope: ["profile", "email"], display: "popup" })(req, res);
+})
 
 //Once the user is verified, return to site
-router.route("/callback").get(passport.authenticate('google', { successRedirect: '/auth/google/profile', failureRedirect: '/', failureFlash: 'Invalid login' }))
+router.get("/callback", (req, res) => {
+  passport.authenticate('google', { successRedirect: '/auth/google/success', failureRedirect: '/login' })(req, res)
+})
 
-//Redirect the user to their profile page
-router.route("/profile").get(isLoggedIn, authController.profile);
+//Route auth/google/success
+router.get("/success", (req, res) => {
+  console.log("SUCCESS SESSION PASS USER: ", req.session.passport.user);
+  console.log("SUCCESS REQ USER: ", req.user);
+  console.log("SUCCESS REQ SESSION USER: ", req.session);
+  //
+  if (req.user && req.user.profileID) {
+    let string = serialize(req.user);
+    res.redirect(`https://esarnb.github.io/venvi-fe/success`);  
+    // res.redirect(`https://esarnb.github.io/venvi-fe/success/${string}`);  
+    // res.cookie("userid", req.user.profileID, {domain: "esarnb.github.io"})
+    // res.cookie("authenticated", true, {domain: "esarnb.github.io"});
+  }
+})
 
-//If the user hits log out btn, run logout
-router.route("/logout").get(authController.logout);
+// //First the button is hit to update passport
+// router.route("/login").get( (req, res) => {
+//   passport = req.app.get('passport')
+//   res.redirect("/auth/google")
+// })
 
-//Check if the user is already logged in or not
-function isLoggedIn(req, res, next) {
-  // if user is authenticated in the session, carry on
-  if (req.isAuthenticated()) return next();
-  // if they aren't redirect them to home or signins
-  res.redirect('/');
-}
+// //Then the user redirects to youtube
+// router.route("/").get(
+//   passport.authenticate('google', { scope: ["profile", "email"] })
+// )
+
+// //Once the user is verified, return to site
+// router.route("/callback").get(passport.authenticate('google', { successRedirect: '/auth/google/profile', failureRedirect: '/', failureFlash: 'Invalid login' }))
+
+// //Redirect the user to their profile page
+// router.route("/profile").get(isLoggedIn, authController.profile);
+
+// //If the user hits log out btn, run logout
+// router.route("/logout").get(authController.logout);
+
+// //Check if the user is already logged in or not
+// function isLoggedIn(req, res, next) {
+//   // if user is authenticated in the session, carry on
+//   if (req.isAuthenticated()) return next();
+//   // if they aren't redirect them to home or signins
+//   res.redirect('/');
+// }
 
 module.exports = router;
